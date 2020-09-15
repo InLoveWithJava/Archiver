4335d2ad70a112fc354038240308bc7a5927b3f3
InLoveWithJava <javalang@list.ru>
2020-09-15 11:52:05 +0300

    #4 - lvl 4

--- Archiver.java
+++ Archiver.java
@@ -1,19 +1,24 @@
 package com.javarush.task.task31.task3110;
 
-import java.nio.file.Paths;
+import com.javarush.task.task31.task3110.command.ExitCommand;
 
-import static com.javarush.task.task31.task3110.ConsoleHelper.readString;
-import static com.javarush.task.task31.task3110.ConsoleHelper.writeMessage;
+import java.nio.file.Paths;
 
 public class Archiver {
     public static void main(String[] args) {
-        writeMessage("Введите полный путь архива:");
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(readString()));
-        writeMessage("Введите путь к файлу, который будем архивировать:");
         try {
-            zipFileManager.createZip(Paths.get(readString()));
-        } catch (Exception exception) {
-            exception.printStackTrace();
+            ConsoleHelper.writeMessage("Введите полный путь архива:");
+            ZipFileManager zipFileManager = new ZipFileManager(Paths.get(ConsoleHelper.readString()));
+            ConsoleHelper.writeMessage("Введите путь к файлу, который будем архивировать:");
+            try {
+                zipFileManager.createZip(Paths.get(ConsoleHelper.readString()));
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+            ExitCommand exitCommand = new ExitCommand();
+            exitCommand.execute();
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("Ошибка");
         }
     }
 }
--- ConsoleHelper.java
+++ ConsoleHelper.java
@@ -5,36 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConsoleHelper {
-    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
 
     public static void writeMessage(String message) {
         System.out.println(message);
     }
 
-    public static String readString() {
-        String result = null;
-        while (true) {
-            try {
-                result = reader.readLine();
-                break;
-            } catch (IOException e) {
-                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
-            }
-        }
-        return result;
+    public static String readString() throws IOException {
+        String text = bis.readLine();
+        return text;
     }
 
-    public static int readInt() {
-        int result = 0;
-        while (true) {
-            try {
-                result = Integer.parseInt(readString());
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
-            }
-        }
-        return result;
+    public static int readInt() throws IOException {
+        String text = readString();
+        return Integer.parseInt(text.trim());
     }
-
 }
